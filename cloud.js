94fff3efbaddc52e7e780b52fe05b3045182ab70
BenChan <thekey0058@163.com>
2017-11-23 09:38:18 +0800

    Update test bug

--- cloud.js
+++ cloud.js
@@ -3,11 +3,17 @@ var AV = require('leanengine');
 /**
  * 一个简单的云代码方法
  */
-AV.Cloud.define('hello', function(request) {
-  return 'Hello world!';
+AV.Cloud.define('updateRoomStatus', function(request) {
+	var sql = 'UPDATE Rooms set status = "' + request.params.status  + '" where objectId = "' + request.params.objectId  + '"'
+	AV.Query.doCloudQuery(sql).then(function(data) {
+		console.log('SQL: ' + sql);
+		console.log(data.results);
+	}, function(error) {
+		console.error(error);
+	});
 });
 
 
-AV.Cloud.afterUpdate('Rooms', function(reuqest) {
+AV.Cloud.afterUpdate('Rooms', function(request) {
 	console.log('Updated room,the id is :' + request.object.id);
 });
