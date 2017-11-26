aae3cc8c9ee94d613d20bae74832b60f9a2a2ee8
key0058 <thekey0058@163.com>
2017-11-26 22:58:21 +0800

    Update player hook functions

--- cloud.js
+++ cloud.js
@@ -1,7 +1,7 @@
 var AV = require('leanengine');
 
 /**
- * 一个简单的云代码方法
+ * David game room leancloud functions
  */
 AV.Cloud.define('updateRoomStatus', function(request) {
 	var sql = 'UPDATE Rooms set status = "' + request.params.status  + '" where objectId = "' + request.params.objectId  + '"'
@@ -13,6 +13,52 @@ AV.Cloud.define('updateRoomStatus', function(request) {
 	});
 });
 
+AV.Cloud.define('updatePlayerRoomId', function(request) {
+	var sql = 'UPDATE Players set roomId = "' + request.params.roomId  + '" where objectId = "' + request.params.playerId  + '"'
+	AV.Query.doCloudQuery(sql).then(function(data) {
+		console.log('SQL: ' + sql);
+		console.log(data.results);
+	}, function(error) {
+		console.error(error);
+	});
+});
+
+
+
+/**
+ * David game room leancloud hooks
+ */
+AV.Cloud.beforeUpdate('Players', function(request) {
+	if (request.object.updatedKeys.indexOf('roomId') != -1) {
+		var roomId = request.object.get('roomId');
+
+		var roomQuery = new AV.Query('Rooms');
+		roomQuery.equalTo('objectId', roomId);
+		roomQuery.find().then(function(results) {
+			console.log('+++++ find room id +++++');
+			if (results.length > 0) {
+				console.log('+++++ checked room id +++++');
+				var query = new AV.Query('Players');
+				query.equalTo('roomId', roomId);
+				query.count().then(function(count) {
+					console.log("Room " + roomId + " total had " + count + " players");
+				});
+			} else {
+				console.log('+++++ no room id +++++');
+				throw new AV.Cloud.Error("Room " + roomId + " is not exists!");
+			}
+		}, function(error) {
+			throw new AV.Cloud.Error('Before update players had error: ' + error);
+		});
+	}
+});
+
+AV.Cloud.afterUpdate('Players', function(request) {
+	if (request.object.updatedKeys.indexOf('roomId') != -1) {
+		var roomId = request.object.get('roomId');
+		console.log("After update player room id " + roomId);
+	}
+});
 
 AV.Cloud.afterUpdate('Rooms', function(request) {
 	console.log('Updated room,the id is :' + request.object.id);
